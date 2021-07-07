0bd5215d210cb7999ce1e2674b4fc3d447fe49e1
olivierbenoit <olivier.bent@gmail.com>
2021-07-07 14:31:56 +0200

    middleware CORS

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,20 @@ const express = require("express");
 //mon application
 const app = express();
 
+//evite erreur CORS (securite de requette malveillante)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 //tester si serveur Node gère correctement l'application Express
 app.use((req, res) => {
   res.json({ message: "node gere bien l'appli express!" });
