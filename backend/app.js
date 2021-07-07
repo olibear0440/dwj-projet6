110b02b1086e59bde9bf80e4e057e310abba1b7b
olivierbenoit <olivier.bent@gmail.com>
2021-07-07 14:43:21 +0200

    connexion api a mongodb - test de connexion ok

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 //import packages
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 //mon application
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://OlivierBenoit:Tigerwood0440@clustertest.i1tkd.mongodb.net/OlivierOPCR?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 //evite erreur CORS (securite de requette malveillante)
 app.use((req, res, next) => {
@@ -18,10 +28,17 @@ app.use((req, res, next) => {
   next();
 });
 
+//route post
+
+//transformer le json en objet javascript pour tte les routes
+app.use(bodyParser.json());
+
+/*
 //tester si serveur Node gère correctement l'application Express
 app.use((req, res) => {
   res.json({ message: "node gere bien l'appli express!" });
 });
+*/
 
 //export de l'appli vers les autres fichiers
 module.exports = app;
