b283a8e94df3d802bd3889eb0db372e8bf7e7568
olivierbenoit <olivier.bent@gmail.com>
2021-07-07 14:09:45 +0200

    creation du serveur- test de reponse ok

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
+// import des packages
 const http = require("http");
 
+//creation du serveur avec fonction qui renvoie la reponse du serveur
 const server = http.createServer((req, res) => {
   res.end("test de reponse du serveur");
 });
 
+
+// ecoute de la requette- checker la reponse sur localhost:3000
 server.listen(process.env.PORT || 3000);
