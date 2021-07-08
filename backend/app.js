1de789198bbbf5849fef63dcb348ad41535e8d7e
olivierbenoit <olivier.bent@gmail.com>
2021-07-08 11:32:52 +0200

    inmplementation routes - get/ post tte sauces + get/ put/ delete des id

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 //import packages
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Sauce = require("./models/thing");
+//console.log(Sauce)
 
 //mon application
 const app = express();
@@ -28,17 +30,74 @@ app.use((req, res, next) => {
   next();
 });
 
-//route post
-
 //transformer le json en objet javascript pour tte les routes
 app.use(bodyParser.json());
 
-/*
-//tester si serveur Node gère correctement l'application Express
-app.use((req, res) => {
-  res.json({ message: "node gere bien l'appli express!" });
+//envois de tte les sauces
+app.post("/api/sauces", (req, res, next) => {
+  delete req.body.userId; //retirer le id car il va etre creé par mongo
+  const sauce = new Sauce({
+    userId: req.body.userId,
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper,
+    imageUrl: req.body.imageUrl,
+    heat: req.body.heat,
+    likes: req.body.likes,
+    dislikes: req.body.dislikes,
+    usersLiked: req.body.usersLiked,
+    usersDisliked: req.body.usersDisliked,
+  });
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
 });
-*/
+
+//
+app.post("/api/sauces", (req, res, next) => {
+
+  
+})
+
+
+
+
+
+
+
+//Renvoie la sauce avec l'ID fourni
+app.get("/api/sauces/:id", (req, res, next) => {
+  Sauce.findOne({ userId: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+//Met à jour la sauce avec l'identifiant fourni
+app.put("/api/sauces/:id", (req, res, next) => {
+  Sauce.updateOne(
+    { UserId: req.params.id },
+    { ...req.body, userId: req.params.id }
+  )
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+
+//Supprime la sauce avec l'ID fourni
+app.delete("/api/sauces/:id", (req, res, next) => {
+  Sauce.deleteOne({ UserId: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+//Renvoie le tableau de toutes les sauces dans la base de données
+app.use("/api/sauces", (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json({ sauces }))
+    .catch((error) => res.status(400).json({ error }));
+});
 
 //export de l'appli vers les autres fichiers
 module.exports = app;
