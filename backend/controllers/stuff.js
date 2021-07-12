77da69e2c7e3665af538759d12d87766bb944a9d
olivierbenoit <olivier.bent@gmail.com>
2021-07-12 12:24:50 +0200

    correction erreur affichage sur le site

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -51,9 +51,11 @@ exports.deleteSauce = (req, res, next) => {
 
 //afficher ttes les sauces
 exports.getAllSauces = (req, res, next) => {
+  console.log(Sauce);
   Sauce.find()
-    .then((sauces) => res.status(200).json({ sauces }))
+    .then((sauces) => res.status(200).json( sauces ))
     .catch((error) => res.status(400).json({ error }));
+
 };
 
 //afficher une sauce
